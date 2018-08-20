4f0d4cf7e8e005dbd0e985c5b6fcd6e538942bb1
Jordan Mavrogeorge <jordomav@gmail.com>
2018-08-19 21:02:45 -0700

    day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule } from '@angular/material';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,35 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  items: object[] = [
-    { name: 'item 1', status: false, done: false },
-    { name: 'item 2', status: false, done: false },
-    { name: 'item 3', status: true, done: false },
-    { name: 'item 4', status: false, done: false }
-  ];
-  newItem: string;
-
-  constructor() { }
-  toggleStatus(item) {
-    item.status = !item.status;
-  }
-  removeItem(item) {
-    item.done = !item.done;
-  }
-  addItem() {
-    const item: object = {
-      name: this.newItem,
-      status: false,
-      done: false
-    };
-    this.items.push(item);
-    this.newItem = '';
-    return;
-  }
-  deleteItem(item) {
-    // const index = this.items.indexOf(item);
-    this.items.splice(item, 1);
-  }
 
   ngOnInit() {
   }
